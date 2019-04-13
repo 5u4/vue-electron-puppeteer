58c6c03cf77d51d9218f57498f3a5e6db53ad6ea
Senhung <alexwongsenhung@gmail.com>
2019-04-12 19:36:57 -0700

    Initialize

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <google-search></google-search>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import GoogleSearch from "./components/GoogleSearch.vue";
+
+export default Vue.extend({
+  components: {
+    GoogleSearch,
+  },
+});
+</script>
---
+++ src/components/GoogleSearch.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <input v-model="searchContent" @keydown.enter="search" />
+    <button @click="search">Search</button>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import { googleSearch } from "../core/GoogleSearch";
+
+export default Vue.extend({
+  data() {
+    return {
+      searchContent: "",
+    };
+  },
+  methods: {
+    async search() {
+      await googleSearch(this.searchContent);
+      this.searchContent = "";
+    },
+  },
+});
+</script>
